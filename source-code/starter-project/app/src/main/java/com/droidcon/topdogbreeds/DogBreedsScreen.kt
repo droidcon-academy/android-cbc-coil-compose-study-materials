ddc86d9bbada8218184db2760e56ebdafeea27e0
Bkinya <gitongakinya1@gmail.com>
2023-08-10 08:16:30 +0300

    update gradle versions

--- source-code/starter-project/app/src/main/java/com/droidcon/topdogbreeds/DogBreedsScreen.kt
+++ source-code/starter-project/app/src/main/java/com/droidcon/topdogbreeds/DogBreedsScreen.kt
@@ -2,38 +2,60 @@ package com.droidcon.topdogbreeds
 
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.compose.rememberAsyncImagePainter
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import java.lang.Float.min
 
 const val DISK_CACHE_KEY = "dogsImageDisk"
 const val CONTENT_DESCRIPTION = "Top Dog Breed"
+const val ERROR_PLACEHOLDER = "Error Occurred"
 
 // Dog breeds image urls
-const val FRENCH_BULL_DOG = "https://www.akc.org/wp-content/uploads/2021/05/French-Bulldog-puppy-head-portrait-outdoors.jpeg"
-const val GOLDEN_RETRIEVER = "https://www.vidavetcare.com/wp-content/uploads/sites/234/2022/04/golden-retriever-dog-breed-info.jpeg"
-const val GERMAN_SHEPHERD_BOY = "https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"
+const val FRENCH_BULL_DOG =
+    "https://www.akc.org/wp-content/uploads/2021/05/French-Bulldog-puppy-head-portrait-outdoors.jpeg"
+const val GOLDEN_RETRIEVER =
+    "https://www.vidavetcare.com/wp-content/uploads/sites/234/2022/04/golden-retriever-dog-breed-info.jpeg"
+const val GERMAN_SHEPHERD_BOY =
+    "https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"
 
 @Preview
 @Composable
@@ -44,7 +66,7 @@ fun DogBreedsScreen(
         modifier = modifier
             .padding(16.dp)
             .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
@@ -55,34 +77,40 @@ fun DogBreedsScreen(
                 fontWeight = FontWeight.SemiBold
             )
         )
+        val imageRequest =
+            ImageRequest.Builder(LocalContext.current)
+                .data(FRENCH_BULL_DOG)
+                .diskCacheKey(DISK_CACHE_KEY)
+                .diskCachePolicy(CachePolicy.ENABLED)
+                .memoryCachePolicy(CachePolicy.DISABLED)
+                .networkCachePolicy(CachePolicy.ENABLED)
+                .build()
+        AsyncImage(
+            model = imageRequest,
+            contentDescription = CONTENT_DESCRIPTION,
+            placeholder = painterResource(
+                id = R.drawable.ic_downloading
+            ),
+            error = painterResource(id = R.drawable.ic_error)
+        )
 
 
-
-
-
-//       val painterWithTransition =
-//            rememberAsyncImagePainter(model = FRENCH_BULL_DOG)
-//        val stateWithTransition = painterWithTransition.state
+//        val painterState = painter.state
 //        val transition = animateFloatAsState(
-//            targetValue = if (stateWithTransition is AsyncImagePainter.State.Success) 1f else 0f
+//            targetValue = if (painterState is AsyncImagePainter.State.Success) 1f else 0f,
+//            label = ""
 //        ).value
 //
-//        if (stateWithTransition is AsyncImagePainter.State.Loading) {
+//        if (painterState is AsyncImagePainter.State.Loading) {
 //            LoadingAnimation()
 //        }
 //        Image(
-//            painter = painterWithTransition,
+//            painter = painter,
 //            contentDescription = null,
 //            modifier = Modifier
-//                /**
-//                 * Column has unbound height
-//                 * Specify the height for the image so that compose can resolve its size
-//                 * and call [AsyncImagePainter.onDraw]
-//                 */
-//                .height(200.dp)
 //                .scale(.8f + (.5f * transition))
-//                .graphicsLayer { rotationX = (1f - transition) * 40f }
-//                .alpha(min(1f, transition / .2f)),
+//                .graphicsLayer { rotationX = (1f - transition) * 90f }
+//                .alpha(min(1f, transition / .7f)),
 //            colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply { setToSaturation(transition) })
 //        )
     }
@@ -90,14 +118,14 @@ fun DogBreedsScreen(
 
 @Composable
 fun LoadingAnimation() {
-    val animation = rememberInfiniteTransition()
+    val animation = rememberInfiniteTransition(label = "")
     val progress = animation.animateFloat(
         initialValue = 0f,
         targetValue = 1f,
         animationSpec = infiniteRepeatable(
             animation = tween(2000),
             repeatMode = RepeatMode.Restart,
-        )
+        ), label = ""
     ).value
 
     Box(
